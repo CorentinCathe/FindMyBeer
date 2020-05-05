6b64606059df038a7dae76c8365959aa89a1d654
Louis Muzellec <louis.muzellec@etu.univ-nantes.fr>
2020-05-05 11:43:44 +0100

    autocomplete fonctionelle

--- src/components/search.js
+++ src/components/search.js
@@ -1,15 +1,29 @@
 import useApi from "../api.js";
 
 Vue.component('search', {
-    template: `<form  @submit.prevent="handleSubmit">
+    template: `<form  @submit.prevent="handleSubmit" class="input-field">
                     <input type="text" v-model="cityName" id="searchCity" v-on:keyup="handleKeyUp()"/>
                     <input type="submit" value="SEARCH" class="btn"/>
             </form>`,
     data: function() {
         return {
             cityName: "",
-            alreadySearched: []
+            alreadySearched: [],
+            instanceAutoComplete: null,
+            autoCompleteData: {}
         }
+    },
+    mounted() {
+        document.addEventListener("DOMContentLoaded", () => {
+            const searchCityElement = document.querySelector("#searchCity");
+            const options = {
+                minLength: 3,
+                data: this.autoCompleteData,
+                sortFunction: (a, b) => a.localeCompare(b),
+                onAutocomplete: this.onAutoCompleteHandler
+            }
+            this.instanceAutoComplete = M.Autocomplete.init(searchCityElement, options);
+        })
     },
     methods: {
         handleKeyUp: function() {
@@ -18,16 +32,21 @@ Vue.component('search', {
             if (this.cityName.length >= 3 && !this.alreadySearched.includes(this.cityName)) {
                 this.alreadySearched.push(this.cityName);
                 useApi.forAutoCompleted(this.cityName)
-                    .then(data => console.log(data))
+                    .then(data => {
+                        data.predictions.map(predict => {
+                            this.autoCompleteData[predict.description] = null;
+                            this.instanceAutoComplete.updateData(this.autoCompleteData);
+                            this.instanceAutoComplete.open();
+                        })
+                    })
                     .catch(err => console.log(err));
             }
         },
         handleSubmit: function() {
             useApi.bySearch(this.cityName).then(data => this.$emit('search-done', data)).catch((err) => console.log(err));
         },
-        autoComplet: function() {
-            let input = document.getElementById('#searchCity')
-
+        onAutoCompleteHandler: function(data) {
+            console.log(data);
         }
     },
 
